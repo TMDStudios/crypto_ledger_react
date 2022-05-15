09d96b68c85d48db4a0871f4fad0abbc8c62ff17
TMDStudios <tmdstudioscontact@gmail.com>
2022-05-15 15:45:07 -0400

    Cleaned up coin display

--- src/views/Prices.jsx
+++ src/views/Prices.jsx
@@ -4,6 +4,10 @@ import axios from 'axios';
 function Prices(){
     const url = "https://api.nomics.com/v1/currencies/ticker?key=eb3a7d733a1719cd0a2731a28b6c9bedcb052751&per-page=100";
     const [ coinData, setCoinData ] = useState([]);
+    const starOutline = "https://tmdstudios.files.wordpress.com/2022/03/goldstaroutline-1.png";
+    const starFull = "https://tmdstudios.files.wordpress.com/2022/03/goldstar.png";
+    const upArrow = "https://tmdstudios.files.wordpress.com/2022/03/uparrow-1.png";
+    const downArrow = "https://tmdstudios.files.wordpress.com/2022/03/downarrow-1.png";
 
     useEffect(() => {
         axios.get(url)
@@ -18,9 +22,14 @@ function Prices(){
             <div>
                 <br></br>
                 {coinData.map((coin, i) => {
+                    const star = i%3===0 ? starFull : starOutline;
                     return (
-                        <div key={i}>
-                            <span>{coin.rank}, {coin.symbol}, {coin.price}<hr/></span>
+                        <div className='container' key={i}>
+                            <div className='flex-1'><img src={star}/></div>
+                            <div className='flex-1'>{coin.rank}</div>
+                            <div className='flex-1'><object data={coin.logo_url} width="32" height="32"></object></div>
+                            <div className='flex-3'>{coin.name} - {coin.symbol}</div>
+                            <div className='flex-2'>${coin.price}</div>
                         </div>
                     )
                 })}
