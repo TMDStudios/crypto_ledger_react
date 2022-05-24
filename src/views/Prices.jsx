b795f4f9d04de6bda191982b214c1e8af47d3eb9
TMDStudios <tmdstudioscontact@gmail.com>
2022-05-23 20:42:27 -0400

    Switched to Crypto Ledger Spring API

--- src/views/Prices.jsx
+++ src/views/Prices.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Prices(){
-    const url = "https://api.nomics.com/v1/currencies/ticker?key=eb3a7d733a1719cd0a2731a28b6c9bedcb052751&per-page=100";
+    // const url = "https://api.nomics.com/v1/currencies/ticker?key=eb3a7d733a1719cd0a2731a28b6c9bedcb052751&per-page=100";
+    const url = "http://localhost:8080/api/coins"
     const [ coinData, setCoinData ] = useState([]);
     const starOutline = "https://tmdstudios.files.wordpress.com/2022/03/goldstaroutline-1.png";
     const starFull = "https://tmdstudios.files.wordpress.com/2022/03/goldstar.png";
@@ -28,8 +29,8 @@ function Prices(){
                             <div className='flex-1'>
                                 <div><img src={star}/></div>
                             </div>
-                            <div className='flex-1'>{coin.rank}</div>
-                            <div className='flex-1'><object data={coin.logo_url} width="32" height="32"></object></div>
+                            <div className='flex-1'>{coin.coinRank}</div>
+                            <div className='flex-1'><object data={coin.logo} width="32" height="32"></object></div>
                             <div className='flex-3'>{coin.name} - {coin.symbol}</div>
                             <div className='flex-2'>${coin.price}</div>
                         </div>
